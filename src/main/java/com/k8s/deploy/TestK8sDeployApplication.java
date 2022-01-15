15506fb5491ba9b8f046e52297deac0378129204
段守志 <dszazhy1314@163.com>
2022-01-15 17:34:35 +0800

    初始化工程

---
+++ src/main/java/com/k8s/deploy/TestK8sDeployApplication.java
@@ -0,0 +1,13 @@
+package com.k8s.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestK8sDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestK8sDeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/k8s/deploy/TestK8sDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8s.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestK8sDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
